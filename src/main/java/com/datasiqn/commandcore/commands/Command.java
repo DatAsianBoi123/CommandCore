8b930c9a6a182c23c81993faf3b115a83a1fbf8f
DatAsianBoi123 <zach.su158@gmail.com>
2022-10-23 20:52:53 -0600

    separated Commands and CommandExecutors

--- src/main/java/com/datasiqn/commandcore/commands/Command.java
+++ src/main/java/com/datasiqn/commandcore/commands/Command.java
@@ -1,26 +1,16 @@
 package com.datasiqn.commandcore.commands;
 
-import com.datasiqn.commandcore.arguments.Arguments;
-import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface Command {
-    CommandOutput execute(@NotNull CommandSender sender, @NotNull Arguments args);
+    @NotNull CommandExecutor getExecutor();
 
-    @Nullable
-    String getPermissionString();
+    @Nullable String getPermissionString();
 
-    @NotNull
-    String getDescription();
+    @NotNull String getDescription();
 
     List<String> getUsages();
-
-    @NotNull
-    default List<String> tabComplete(@NotNull CommandSender sender, @NotNull Arguments args) {
-        return new ArrayList<>();
-    }
 }
