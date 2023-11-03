940e51fb9565692c883aa52dd4c77b6bbf311a4f
DatAsianBoi123 <zach.su158@gmail.com>
2023-11-03 13:50:36 -0600

    added javadoc

--- src/main/java/com/datasiqn/commandcore/command/CommandContext.java
+++ src/main/java/com/datasiqn/commandcore/command/CommandContext.java
@@ -8,4 +8,12 @@ import org.jetbrains.annotations.NotNull;
  * Represents the context in which a command is executed
  */
 public record CommandContext(@NotNull CommandSource source, @NotNull Command command, @NotNull String label, @NotNull Arguments arguments) {
+    /**
+     * Creates a new command context
+     * @param source The source of the command
+     * @param command The command that is being executed
+     * @param label The label that the user used when executing this command. This could either be the actual command name, or any aliases of the command.
+     * @param arguments The arguments that the command was executed with
+     */
+    public CommandContext { }
 }
